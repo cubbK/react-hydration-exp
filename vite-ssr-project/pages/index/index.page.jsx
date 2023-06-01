c1c6e48fd757b1060bc475f864ceaef2d104cdd3
Dan Popa <dan.popa@hm.com>
2023-06-01 15:07:33 +0200

    add vite micro

---
+++ vite-ssr-project/pages/index/index.page.jsx
@@ -0,0 +1,12 @@
+import { Counter } from "./Counter";
+
+export { Page };
+
+function Page() {
+  return (
+    <>
+      <h1>Micro React</h1>
+      <Counter />
+    </>
+  );
+}
---
+++ vite-ssr-project/renderer/_default.page.server.jsx
@@ -0,0 +1,24 @@
+export { render };
+// See https://vite-plugin-ssr.com/data-fetching
+export const passToClient = ["pageProps", "urlPathname"];
+
+import ReactDOMServer from "react-dom/server";
+import { escapeInject, dangerouslySkipEscape } from "vite-plugin-ssr/server";
+
+async function render(pageContext) {
+  const { Page, pageProps } = pageContext;
+  // This render() hook only supports SSR, see https://vite-plugin-ssr.com/render-modes for how to modify render() to support SPA
+  if (!Page)
+    throw new Error("My render() hook expects pageContext.Page to be defined");
+  const pageHtml = ReactDOMServer.renderToString(<Page {...pageProps} />);
+
+  const documentHtml = escapeInject`
+        <div id="micro-react">${dangerouslySkipEscape(pageHtml)}</div>`;
+
+  return {
+    documentHtml,
+    pageContext: {
+      // We can add some `pageContext` here, which is useful if we want to do page redirection https://vite-plugin-ssr.com/page-redirection
+    },
+  };
+}
